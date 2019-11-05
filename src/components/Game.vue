a3044ec421adecc3c18d8843a83759d5d9fc0a44
Napermial <hatvani98@freemail.hu>
2019-11-05 18:56:56 +0100

    ticking

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
         <svg width="1000" height="500">
-            <rect class="mario" width="50" height="100" stroke="10" v-bind:x="xPos"  y="400"></rect>
-            <rect class="luigi" width="50" height="100" stroke="10" x="800" y="400" ></rect>
+            <rect class="mario" width="50" height="100" stroke="10" x="50"  v-bind:y="playerPos"></rect>
+            <rect class="luigi" width="50" height="100" stroke="10" v-bind:x="obstaclePos" y="400" ></rect>
             <line class="line" x1="0" y1="500" x2="1000"  y2="500"></line>
             <circle class="sun" r="50" cx="100" cy="100" ></circle>
             <rect class="enemy" height="50" width="50" x="600" y="450"  ></rect>
@@ -16,20 +16,30 @@
         data() {
             return {
                 speed: 1,
-                xPos:700
+                playerPos:400,
+                playerWeight: 80,
+                playerSpeed:1,
+                playerJumpStrength:200,
+                gravity:9.89,
+                playerJumping:false,
+                obstaclePos:800
             }
         },
         created() {
-         document.addEventListener('keydown', event => this.handleUserAction(event))
+         document.addEventListener('keydown', event => this.handleUserAction(event));
+             this.start()
          }
         ,methods:{
                 nextFrame(){
-
+                    this.obstaclePos -= this.playerSpeed
                 },
             handleUserAction(event){
                 if(event.keyCode === 32){
-                    this.xPos++;
+                    this.playerPos -=20
                 }
+            },
+            start(){
+                setInterval(this.nextFrame, 40);
             }
         }
     }
