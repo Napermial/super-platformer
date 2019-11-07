68d4a5868c6037aa46358e2cc7e7fcbe8bf11496
Napermial <hatvani98@freemail.hu>
2019-11-07 15:42:49 +0100

    form send

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,22 +3,25 @@
         <h1>SUPRE-A PLADFROMA GME</h1>
         <h2>TRY-A NUW FREE</h2>
         <div>
-            <input placeholder="PLAYER NAME" v-bind="playerName">
+            <label>
+                <input placeholder="PLAYER NAME" v-model="playerName">
+            </label>
         </div>
         <div>
-            <label>SELECT-A MARIO</label>
-            <input type="radio" name="mario">
+            <label> SELECT-A MARIO
+                <input type="radio" v-model="isMario" value="MARIO">
+            </label>
             <svg x="100" y="50">
                 <rect  width="50" height="100" style="fill: red" x="0" y="0"></rect>
             </svg>
-            <label>SELECT-A LUIGI</label>
-            <input type="radio" name="luigi">
+            <label> SELECT-A LUIGI
+                <input type="radio"  v-model="isMario" value="LUIGI">
+            </label>
             <svg x="100" y="50">
                 <rect  width="50" height="100" style="fill: green" x="0" y="0"></rect>
             </svg>
         </div>
         <button v-on:click="sendCredentials">PLAY</button>
-
     </div>
 </template>
 
@@ -29,12 +32,16 @@
         name: "Main",
         data() {
             return {
-                playerName:null
+                playerName:null,
+                isMario:null
             }
         },
         methods: {
             sendCredentials(){
-                axios.post()
+                axios.post('http://localhost:8762/player/start-game',{
+                    'name':this.playerName, 'character': this.isMario
+                });
+                this.$router.push('/game')
             }
         }
     }
